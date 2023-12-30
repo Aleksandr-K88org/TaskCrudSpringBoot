4ed537de7c065bde6a6d763883bbec9d524da57c
Aleksandr-K88 <bon290688@mail.ru>
2023-12-30 16:12:51 +0300

    TaskCrud

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,48 +1,48 @@
-//package web.dao;
-//
-//import org.springframework.stereotype.Repository;
-//import org.springframework.transaction.annotation.Transactional;
-//import web.model.User;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Repository
-//@Transactional(readOnly = true)
-//public class UserDaoImpl implements UserDao {
-//
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//
-//    @Transactional
-//    @Override
-//    public void save(User user) {
-//        entityManager.persist(user);
-//    }
-//
-//    @Override
-//    public List<User> findAll() {
-//        return entityManager.createQuery("select u from User u", User.class).getResultList();
-//    }
-//
-//    @Override
-//    public Optional<User> findById(Long id) {
-//        User user = entityManager.find(User.class, id);
-//        return Optional.ofNullable(user);
-//    }
-//
-//    @Transactional
-//    @Override
-//    public void updateUser(User user) {
-//        entityManager.merge(user);
-//    }
-//
-//    @Transactional
-//    @Override
-//    public void deleteById(Long id) {
-//        Optional<User> userById = findById(id);
-//        userById.ifPresent(user -> entityManager.remove(user));
-//    }
-//}
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        User user = entityManager.find(User.class, id);
+        return Optional.ofNullable(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Long id) {
+        Optional<User> userById = findById(id);
+        userById.ifPresent(user -> entityManager.remove(user));
+    }
+}
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,50 +1,50 @@
-//package web.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import web.dao.UserDao;
-//import web.model.User;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//@Transactional(readOnly = true)
-//public class UserServiceImpl implements UserService {
-//
-//    private final UserDao userDao;
-//
-//    @Autowired
-//    public UserServiceImpl(UserDao userDao) {
-//        this.userDao = userDao;
-//    }
-//
-//    @Transactional
-//    @Override
-//    public void save(User user) {
-//        userDao.save(user);
-//    }
-//
-//    @Override
-//    public List<User> findAll() {
-//        return userDao.findAll();
-//    }
-//
-//    @Override
-//    public Optional<User> findById(Long id) {
-//        return userDao.findById(id);
-//    }
-//
-//    @Transactional
-//    @Override
-//    public void updateUser(User user) {
-//        userDao.updateUser(user);
-//    }
-//
-//    @Transactional
-//    @Override
-//    public void deleteById(Long id) {
-//        userDao.deleteById(id);
-//    }
-//}
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional
+    @Override
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userDao.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Long id) {
+        userDao.deleteById(id);
+    }
+}
