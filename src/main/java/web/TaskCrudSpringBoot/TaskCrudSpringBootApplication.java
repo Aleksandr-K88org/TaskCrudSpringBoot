275c0279150c82dd00dc33bc6bcf6f4784cc4852
Aleksandr-K88 <bon290688@mail.ru>
2024-01-14 15:12:56 +0300

    TaskCrudSpringBoot

---
+++ src/main/java/web/TaskCrudSpringBoot/TaskCrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package web.TaskCrudSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCrudSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/web/TaskCrudSpringBoot/controller/UserController.java
@@ -0,0 +1,77 @@
+package web.TaskCrudSpringBoot.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.TaskCrudSpringBoot.model.User;
+import web.TaskCrudSpringBoot.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String allUsers(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users/list";
+    }
+
+    @GetMapping("/new")
+    public String createUserForm(@ModelAttribute("user") User user) {
+        return "users/create";
+    }
+
+    @PostMapping
+    public String createUser(@ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "users/create";
+        }
+
+        userService.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String editUserForm(@RequestParam("id") Long id, Model model) {
+        Optional<User> userById = userService.findById(id);
+
+        if (userById.isPresent()) {
+            model.addAttribute("user", userById.get());
+            return "users/edit";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/edit")
+    public String editUser(@ModelAttribute("user") @Valid User user,
+                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "users/edit";
+        }
+
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/";
+    }
+}
